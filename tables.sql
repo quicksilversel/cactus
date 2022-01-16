9bbdb49f596dee38cb1d044c08cfb43fc60be5ce
quicksilversel <quicksilversel@gmail.com>
2022-01-16 15:16:18 +0900

    update tables.sql

--- tables.sql
+++ tables.sql
@@ -23,6 +23,16 @@ CREATE TABLE plantData (
     habitat varchar(255) NOT NULL,
 );
 
+CREATE TABLE users (
+  id int(11) AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  username varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  role enum('Author','Admin') DEFAULT NULL,
+  password varchar(255) NOT NULL,
+  created_at timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NULL DEFAULT NULL
+);
+
 -- sample values to plants
 INSERT INTO plant VALUES 
 (1, 'cactus', 20, 18, 1, 0, '2021-11-13'),
